2f44c5eeed9d3e0817b7591e7561bb23a8479f7b
prakevents <prakevent@gmail.com>
2023-07-08 22:21:32 +0200

    removing nodes

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import {
 
 const firebaseConfig = {
   apiKey: "AIzaSyBeKGUZwCOGLll9faEktxJ33_L1gpauUV8",
-  authDomain: "book-event-4e4c4.firebaseapp.com",
+  authDomain: "auth-firebase-try.netlify.app",
   projectId: "book-event-4e4c4",
   storageBucket: "book-event-4e4c4.appspot.com",
   messagingSenderId: "79063146007",
